7070381fb91ebc29cb8f3744e957beecf6019c0e
James Craig <jamespcraig@gmail.com>
2019-03-30 16:11:48 -0400

    revisions to about, back, chest, shoulders, sources

--- views/pages/shoulders.php
+++ views/pages/shoulders.php
@@ -1,14 +1,18 @@
 <h1>Open, Relaxed Shoulders</h1>
-<p>The third key element of good meditation posture is to have open, relaxed shoulders.  This may seem difficult at first, because your shoulders may want to naturally round forward.  However, if the muscles of the chest are sufficiently relaxed, the shoulders will remain back.  </p>
 
-<p>The important thing to understand here, is that the shoulder can and should open without lifting the chest.  This is one of the most common mistakes for beginning practitioners.  In order to bring the shoulders back, the beginning practitioner will unconsciously lift the chest up, which increases the arch in the lower back.  This does have the effect of rotating the shoulders back, and it does produce the appearance of good posture.  However, it does not actually solve the underlying problem of slumped shoulders.</p>
+<p>Your shoulders should be open and relaxed. This may seem difficult at first, because your shoulders may want to naturally round forward. However, if the muscles of the chest are sufficiently relaxed, the shoulders will remain open.</p>
 
-<p>In order to bring the shoulders back, one should do a shoulder roll.  Lift the shoulders up, pull them back, and let them settle down.  Do all of this without lifting the chest.</p>
+<h2>The Shoulder Roll</h2>
 
-<p>Do not,however, pull the shoulders down.  Also do not try to hold the shoulders back.  Just let them rest in an open position.  It may take some time to get a feel for this, and the difference between slumped and open shoulders can feel quite subtle.  However, it does have a dramatic effect on one's comfort while sitting.</p>
+<p>In order to bring the shoulders back, one should do a shoulder roll. Lift the shoulders up, pull them back, and let them settle down. Do all of this without lifting the chest.</p>
 
-<p>It may be difficult at first to open the shoulders if the chest muscles are tight.  For this reason, I highly recommend doing the doorway stretch (see <a href="exercises">Exercises</a>) every day until the chest becomes more open.  Thereafter, continue doing the doorway stretch regularly for maintenance.</p>
+<p>The important thing to understand here is that the shoulders can open without allowing the chest to rise. This is one of the most common sources of confusion for beginning practitioners. In order to bring the shoulders back, the beginning practitioner will unconsciously lift the chest up, which increases the arch in the lower back. While this does have the effect of rotating the shoulders back, and does produce the appearance of good posture, it does not actually solve the underlying problem of slumped shoulders.</p>
 
-<p>One mistake to avoid is pulling the shoulders back and holding them back with effort.  For some people, particularly if their chest muscles are tight, this may seem like the only way to keep the shoulders back.  However, it is better to let the shoulders round than to hold them back with tension.  Instead of holding the shoulders back, one should focus on stretching out the chest.</p>
+<h2>Troubleshooting</h2>
 
-<p>The shoulders should not be shrugged upward or pulled down.  They should simply “float” in their place.  Once they are rolled back, let them rest.</p>
+<p>Some people may have trouble keeping their shoulders completely open while siting with their hands in their lap. That's okay, it's not the end of the world. As with the chest, the shoulders should pretty much take care of themselves, as long as the pelvis, hands, and head are well positioned.</p>
+
+<p>Do not actively pull the shoulders down. Just let them rest in a natural position. Also do not actively hold the shoulders back. This will create unnecessary muscle tension between your shoulder blades.</p>
+
+<p>It is difficult to open the shoulders if the chest muscles are tight. For this reason, I highly recommend doing the doorway stretch (see <a href="exercises">Exercises</a>) every day until the chest becomes more open. Thereafter, continue doing the doorway stretch regularly to keep the chest and shoulders flexible.</p>
+
